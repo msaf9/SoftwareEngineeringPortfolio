bc62fc027a033efc93a1646cf504cd6a2d34c40b
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-05-05 14:50:29 +0530

    Created all components and routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WritingsComponent } from './writings/writings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'writings', component: WritingsComponent },
+  { path: 'hobbies', component: HobbiesComponent },
+  { path: 'bookshelf', component: BookshelfComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WritingsComponent } from './writings/writings.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    PortfolioComponent,
+    WritingsComponent,
+    HobbiesComponent,
+    BookshelfComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
