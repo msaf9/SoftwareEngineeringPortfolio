fa7bc2206b60b8ff69510aca6b220fff3cb76458
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-05-05 17:57:22 +0530

    Implemented Page Not Found component and changed font family

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  visitedRoute = '/';
+  URLS = ['/home', '/portfolio', '/writings', '/hobbies', '/bookshelf'];
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(async (routes) => {
+      if (routes instanceof NavigationEnd) {
+        this.visitedRoute = this.router.url;
+      }
+      this.visitedRoute = this.router.url;
+      if (this.visitedRoute !== this.router.url) {
+        this.visitedRoute = this.router.url;
+      }
+      // this.isVisited(this.visitedRoute);
+    });
+  }
+
+  isVisited(url: string): boolean {
+    console.log(this.URLS.every(urls => urls === url));
+    return this.URLS.every(urls => urls !== url);
+  }
+
+  offNavbarOnPageNotFound(): boolean {
+    return !this.isVisited(this.visitedRoute);
+  }
+
 }
