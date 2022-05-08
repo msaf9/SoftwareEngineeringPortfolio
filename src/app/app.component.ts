e60c9ddd4b17b9ce5303945084da8a42a13b39d1
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-05-09 00:24:13 +0530

    Static code analysis implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
@@ -6,7 +6,7 @@ import { NavigationEnd, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Portfolio';
 
   visitedRoute = '/';
