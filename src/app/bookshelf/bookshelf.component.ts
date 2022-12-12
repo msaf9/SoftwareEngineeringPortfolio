8f3a62903dd3b69e7a57ccd9af33cce65e94143b
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-12-12 15:22:06 +0530

    Formatted using Prettier

--- src/app/bookshelf/bookshelf.component.ts
+++ src/app/bookshelf/bookshelf.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bookshelf',
   templateUrl: './bookshelf.component.html',
-  styleUrls: ['./bookshelf.component.css']
+  styleUrls: ['./bookshelf.component.css'],
 })
 export class BookshelfComponent implements OnInit {
-
   /**
    * hidden by default
    */
@@ -23,20 +22,18 @@ export class BookshelfComponent implements OnInit {
   /**
    * constructor is put into use to inject dependencies into the component class.
    */
-  constructor() { }
+  constructor() {}
 
   /**
-  * ngOnInit is invoked only once when the directive is instantiated.
-  */
-  ngOnInit(): void {
-  }
+   * ngOnInit is invoked only once when the directive is instantiated.
+   */
+  ngOnInit(): void {}
 
   /**
-   * toggleShow method will switch the text to Show or Hide and will show or hide the HTML element respectively. 
+   * toggleShow method will switch the text to Show or Hide and will show or hide the HTML element respectively.
    */
   toggleShow(): void {
     this.showHide = !this.showHide;
     this.isShown = !this.isShown;
   }
-
 }
