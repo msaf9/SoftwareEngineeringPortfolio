44f5bc8774fd8e338b36eaa5353ca53a429ef0fa
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-12-13 16:44:14 +0530

    Formatted using Prettier

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -6,19 +6,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hobbies',
   templateUrl: './hobbies.component.html',
-  styleUrls: ['./hobbies.component.css']
+  styleUrls: ['./hobbies.component.css'],
 })
 export class HobbiesComponent implements OnInit {
-
   /**
    * constructor is put into use to inject dependencies into the component class.
    */
-  constructor() { }
+  constructor() {}
 
   /**
-  * ngOnInit is invoked only once when the directive is instantiated.
-  */
-  ngOnInit(): void {
-  }
-
+   * ngOnInit is invoked only once when the directive is instantiated.
+   */
+  ngOnInit(): void {}
 }
