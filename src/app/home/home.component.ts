bd5e74750aba3dc9109b3255ab2f02989616e4ea
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-12-14 16:20:40 +0530

    Formatted using Prettier

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,19 +6,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   /**
    * constructor is put into use to inject dependencies into the component class.
    */
-  constructor() { }
+  constructor() {}
 
   /**
-  * ngOnInit is invoked only once when the directive is instantiated.
-  */
-  ngOnInit(): void {
-  }
-
+   * ngOnInit is invoked only once when the directive is instantiated.
+   */
+  ngOnInit(): void {}
 }
