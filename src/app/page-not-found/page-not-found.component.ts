a79be70dfb3c9d85dd8bd2a374ac2f600dd029d6
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-12-26 15:49:07 +0530

    Formatted using Prettier

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -6,19 +6,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.css'],
 })
 export class PageNotFoundComponent implements OnInit {
-
   /**
    * constructor is put into use to inject dependencies into the component class.
    */
-  constructor() { }
+  constructor() {}
 
   /**
-  * ngOnInit is invoked only once when the directive is instantiated.
-  */
-  ngOnInit(): void {
-  }
-
+   * ngOnInit is invoked only once when the directive is instantiated.
+   */
+  ngOnInit(): void {}
 }
