24119f0ae906b2029e88488158f4c1c78d58b48c
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-12-27 16:04:45 +0530

    Formatted using Prettier

--- src/app/sitemap/sitemap.component.ts
+++ src/app/sitemap/sitemap.component.ts
@@ -6,19 +6,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sitemap',
   templateUrl: './sitemap.component.html',
-  styleUrls: ['./sitemap.component.css']
+  styleUrls: ['./sitemap.component.css'],
 })
 export class SitemapComponent implements OnInit {
-
   /**
    * constructor is put into use to inject dependencies into the component class.
    */
-  constructor() { }
+  constructor() {}
 
   /**
    * ngOnInit is invoked only once when the directive is instantiated.
    */
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
